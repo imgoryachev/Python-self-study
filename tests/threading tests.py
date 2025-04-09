3d60ab66e60fa7be4d77e3ce6468be2a2cf79509
imgoryachev <bloodon@yandex.ru>
2025-04-09 13:33:11 +0400

    restore old days

--- tests/threading tests.py
+++ tests/threading tests.py
@@ -1,19 +1,26 @@
 import threading
 import time
 
+c = 100
+
 def count20():
     a = 1
+    global c
     while a < 21:
-        print(a)
+        print('a = ', a)
+        if a == 10:
+            c = 15
         time.sleep(1)
         a = a + 1
 
 def count02():
-    a = 20
-    while a > 0:
-        print(a)
+    global c
+    b = 1
+    while b < c:
+    #while a > 0:
+        print('b = ', b)
         time.sleep(1)
-        a = a - 1
+        b = b + 1
 
 inc = threading.Thread(target=count20)
 dec = threading.Thread(target=count02)
@@ -23,9 +30,9 @@ dec.setName("decThread")
 inc.start()
 dec.start()
 
-time.sleep(4)
+#time.sleep(4)
 
-print(threading.enumerate())
+#print(threading.enumerate())
 # print(threading.active_count())
 
 # count20()
---
+++ windowed apps/simple_window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Window")
+label = tk.Label(root, text="Hi")
+label.pack()
+root.mainloop()
